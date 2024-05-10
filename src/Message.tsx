380417736afa2c7ec84e12224ee7461d784380e0
yusuf abdulsalam <eminentjs@gmail.com>
2024-05-09 23:13:42 -0400

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+// PascalCasing: where every first letter is capitalized
+function Message() {
+  // jsx: javascript XML
+  const name = '';
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello World</h1>;
+}
+
+export default Message;
